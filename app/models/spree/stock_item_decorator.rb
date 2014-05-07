738f2a2d450648f18c4b0cb0055534fa46245213
Gregor MacDougall <gregor@freerunningtechnologies.com>
2014-05-07 14:14:51 -0700

    Removed some unused files.

--- app/models/spree/stock_item_decorator.rb
+++
@@ -1,9 +0,0 @@
-Spree::StockItem.class_eval do
-
-  def count_on_hand_at(timestamp = Time.now)
-      stock_movements = Spree::StockMovement.where(stock_item_id: self.id,
-                                                   created_at: timestamp..Time.now)
-      self.count_on_hand - stock_movements.sum(:quantity)
-  end
-
-end
